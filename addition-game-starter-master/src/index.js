0cd11e4913b816e340ff789849ad0196dba37e19
kjw1134 <51304823+kjw1134@users.noreply.github.com>
2021-11-25 17:03:25 +0900

    211125

--- addition-game-starter-master/src/index.js
+++ addition-game-starter-master/src/index.js
@@ -4,108 +4,74 @@ const config={
   rpcURL:'https://api.baobab.klaytn.net:8651'
 }
 
+const contract_address='0xb3e1abb61996d2b5d6a712d2156dfe154edcc451';
 const cav = new Caver(config.rpcURL);
                                         //bApp내 사용가능 전역상수
-const agContract = new cav.klay.Contract(DEPLOYED_ABI, DEPLOYED_ADDRESS);
+const agContract = new caver.klay.Contract(DEPLOYED_ABI, DEPLOYED_ADDRESS);
 const App = {
 
-  auth:{
-    accessType : 'keystore',
-    keystore:'',
-    password:''
+  
+
+  setSendPram:function(_sendParam){
+    this.sendParam = _sendParam;
   },
 
   start: async function () {
+    this.handleLogin();
 
-  },
-
-  // handleImport: async function () {
-  //   const fileReader = new FileReader();
-  //   fileReader.readAsText(event.target.files[0]);
-  //   fileReader.onload = (event )=>{
-  //     try{
-  //       if(!this.checkValidKeystore(event.target.result)){
-  //           $('#message').text('invalid keystore file');
-  //           return;
-  //       }
-        
-  //       this.auth.keystore = evnet.target.result;
-  //       $('#message').text('valid keystore file\n input password');
-  //       document.querySelector('#input-password').focus();
-
-  //     }catch(event){
-  //       $('#message').text('invalid keystore file');
-  //       return;
-  //     }
-  //   }
-  // },
-
-  handlePassword: async function () {
-    this.auth.password = evnet.target.value;
   },
 
   handleLogin: async function () {
 
+    const { klaytn } = window;
+    if (klaytn === undefined) {
+      alert("kiakas must be installed");
+      return;
+    }
+
     try { 
        const accounts = await klaytn.enable();
+       if(accounts)
+       {
+        $('#logout').show();
+        $('#login').hide();
+       }
+        
+       
        //$('#wallet-address').text('address : ' + accounts);
        this.changeUI(accounts);
 
      } catch (error) {  
       $('#wallet-address').text('connect wallet failed!');
       }
-
-     
-    // if(this.auth.accessType=='keystore')
-    // {
-    //   try{
-    //     const privatekey = cav.klay.accounts.decrypt(this.auth.keystore, this.auth.password).privateKey;
-    //     this.integrateWallet(privatekey)
-    //   }
-    //   catch (e){
-    //     $('#message').text('password is not correct');
-    //   }
-    // } 
   },
 
-  // handleLogout: async function () {
-  //   this.removeWallet();
-  //   location.reload();
-  // },
-
-  generateNumbers: async function () {
-    var num1 = Math.floor((Math.random()*50) +10);//0~1 소수점
-    var num2 = Math.floor((Math.random()*50) +10);//0~1 소수점
-
-    sessionStorage.setItem('result', num1+num2);
-
-    $('#start').hide();
-    $('#num1').text(num1);
-    $('#num2').text(num2);
-    $('#qeustion').show();
-
-    document.querySelector('#answer').focus();
-
-    this.showTimer();
-
+  
+  handleLogout: async function () {
+    this.removeWallet();
+    location.reload();
   },
 
-  submitAnswer: async function () {
-    const result = sessionStorage.getItem('result');
-    var answer = $('#answer').val();
+  handleSign: async function (_from,_to,_value,_gas) {
 
-    if(answer==result){
-      if(confirm("0.1 klay 받기")){
-        if(await this.callContractBalance() >= 0.1){
-          this.receiveKlay();
-        }else{
-          alert("klay=0");
-        }
-      }
-    }
-    else{
-      alert("fail");
-    }
+    console.log(klaytn.selectedAddress);
+
+    caver.klay.sendTransaction({
+      from:_from,
+      to:_to,
+      value:cav.utils.toPeb(_value.toString(), 'KLAY'),
+      gas:_gas
+    })
+      .once('transactionHash', (transactionHash) => {
+        console.log('txHash', transactionHash)
+      })
+      .once('receipt', (receipt) => {
+        console.log('receipt', receipt)
+      })
+      .once('error', (error) => {
+        console.log('error', error)
+      });
+    
   },
 
   deposit: async function () {
@@ -115,31 +81,31 @@ const App = {
     const walletInstance = String(this.getWallet());
     const callowner = await this.callOwner();
 
-    
-
       if(walletInstance){
         if(callowner.toUpperCase() != walletInstance.toUpperCase()) return;
         else{
+
           var amount = $('#amount').val();
 
           if(amount){
+            
             agContract.methods.deposit().send({
+             
               from:callowner,//비앱네 계정인증 완료된 계정만 사용가능
+             // to:agContract._address,
               value:cav.utils.toPeb(amount, "KLAY"),
               gas:'250000'
             })
-            // .once('transactionHash', (transactionHash)=> {
-            //   console.log(`txHash: ${transactionHash}`);
-            // })
-            // .once('receipt', (receipt)=>{
-            //   console.log(`(#${receipt.blockNuber})`, receipt);
-            //   spinner.stop();
-            //   alert(amount +'KLAY 컨트렉에 송급했습니다.');
-            //   location.reload(); // 페이지 새로고침
-            // })
-            // .once('error', (error)=>{
-            //   alert(error.message);
-            // });
+            .once('receipt', (receipt) => {
+              console.log('receipt', receipt)
+              spinner.stop();
+              alert(amount +'KLAY 컨트렉에 송급했습니다.');
+              location.reload(); // 페이지 새로고침
+            })
+            .once('error', (error) => {
+              alert(error.message);
+            });
+
             spinner.stop();
           }
           return;
@@ -150,6 +116,41 @@ const App = {
         spinner.stop();
       }
   },
+  
+   receiveKlay: function () {
+
+    var spinner = this.showSpinner();
+    const walletInstance = this.getWallet();
+    if(!walletInstance) return;
+
+    const value = '0.5';
+    //this.handleSign(agContract._address,walletInstance,value,'250000');
+
+    agContract.methods.transfer(caver.utils.toPeb(value, 'KLAY')).send({
+      from:walletInstance,
+      gas:'250000'
+    }).then(function(receipt){
+      if(receipt.status){
+        spinner.stop();
+        alert("0.5Klay transfer");
+        $('#transaction').html("");
+        $('#transaction')
+        .append(`<p><a href= 'https://baobab.scope.klaytn.com/tx/${receipt.transactionHash}' 
+        target='_blank'> Klaytn scope </a></p>`);
+
+        return agContract.methods.getBalance().call()
+        .then(function(balance){
+          $('#contractBalance').html("");
+          $('#contractBalance')
+          .append('<p>' + 'balance : ' + cav.utils.fromPeb(balance, "KLAY")+'KLAY' + '</p>');
+        })
+
+      }
+    })
+  },
+  
+
+ 
 
   callOwner: async function () {
     return await agContract.methods.owner().call();
@@ -169,32 +170,6 @@ const App = {
     }
   },
 
-  // checkValidKeystore: function (keystore) {
-  //   const parsedKeystore = JSON.parse(keystore);
-  //   const isValidKeystore = parsedKeystore.version &&
-  //   parsedKeystore.id &&
-  //   parsedKeystore.address &&
-  //   parsedKeystore.crypto;
-
-  // return isValidKeystore;
-  // },
-
-  // integrateWallet: function (privateKey) {
-  //   const walletInstance = cav.klay.accounts.privateKeyToAccount(privatekey);
-  //   cav.klay.accounts.wallet.add(walletInstance);
-
-  //   //페이지에 저장
-  //   sessionStorage.setItem('wallterInstance', JSON.stringify(walletInstance));
-
-  //   this.changeUI(walletInstance);
-  // },
-
-  reset: function () {
-    this.auth = {
-      keystore:'',
-      password:'',
-    };
-  },
 
   changeUI: async function (walletInstance) {
 
@@ -215,11 +190,18 @@ const App = {
      }
   },
 
-  // removeWallet: function () {
-  //   cav.klay.accounts.wallet.clear();
-  //   sessionStorage.removeItem('walletInstance');
-  //   this.reset();
-  // },
+  showSpinner: function () {
+    var target = document.getElementById("spin");
+    return new Spinner(opts).spin(target)
+  },
+
+  
+
+
+  removeWallet: function () {
+    //klaytn.disable();
+    //this.reset();
+  },
 
   showTimer: function () {
     var sec = 10;
@@ -240,40 +222,47 @@ const App = {
     },1000);
   },
 
-  showSpinner: function () {
-    var target = document.getElementById("spin");
-    return new Spinner(opts).spin(target)
+
+  submitAnswer: async function () {
+    const result = sessionStorage.getItem('result');
+    var answer = $('#answer').val();
+
+    if(answer==result){
+      if(confirm("0.1 klay 받기")){
+        if(await this.callContractBalance() >= 0.1){
+          this.receiveKlay();
+        }else{
+          alert("klay=0");
+        }
+      }
+    }
+    else{
+      alert("fail");
+    }
   },
 
-  receiveKlay: function () {
-    var spinner = this.showSpinner();
+  generateNumbers: async function () {
+    var num1 = Math.floor((Math.random()*50) +10);//0~1 소수점
+    var num2 = Math.floor((Math.random()*50) +10);//0~1 소수점
 
-    const walltetInstance = this.getWallet();
+    sessionStorage.setItem('result', num1+num2);
 
-    if(!walletInstance) return;
+    $('#start').hide();
+    $('#num1').text(num1);
+    $('#num2').text(num2);
+    $('#qeustion').show();
 
-    agContract.methods.transfer(cav.utils.toPeb("0.1", "KLAY")).send({
-      from:walletInstance,
-      gas:'250000'
-    }).then(function(receipt){
-      if(receipt.status){
-        spinner.stop();
-        alert("0.1Klay transfer");
-        $('#transaction').html("");
-        $('#transaction')
-        .append(`<p><a href= 'https://baobab.scope.klaytn.com/tx/${receipt.transactionHash}' 
-        target='_blank'> Klaytn scope </a></p>`);
+    document.querySelector('#answer').focus();
 
-        return agContract.methods.getBalance().call()
-        .then(function(balance){
-          $('#contractBalance').html("");
-          $('#contractBalance')
-          .append('<p>' + 'balance : ' + cav.utils.fromPeb(balance, "KLAY")+'KLAY' + '</p>');
-        })
+    this.showTimer();
 
-      }
-    })
+  },
+
+  handlePassword: async function () {
+    //this.auth.password = evnet.target.value;
   }
+
+
 };
 
 window.App = App;
@@ -281,6 +270,7 @@ window.App = App;
 window.addEventListener("load", function () {
   App.start();
 });
+
 
 var opts = {
   lines: 10, // The number of lines to draw
